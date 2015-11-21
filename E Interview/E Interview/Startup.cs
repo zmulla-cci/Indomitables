735af7a8b9ac03bc1e3cd4df69373e77ea059c3c
Zohaib Mulla <zohaib.mulla@creativecapsule.com>
2015-11-21 11:57:51 +0530

    Initial Code commit

---
+++ E Interview/E Interview/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Interview.Startup))]
+namespace E_Interview
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
